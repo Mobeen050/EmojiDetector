17193e0808ffbba5b88b3e985d26bdf9042515d7
Mobeen-Dev <mubeenqamar29@gmail.com>
2025-03-27 15:02:18 +0500

    Code to get img from dataset

---
+++ base64_to_img.py
@@ -0,0 +1,25 @@
+import io
+import base64
+import pandas as pd
+from PIL import Image
+
+def base64_to_image(base64_string):
+    base64_string = base64_string[22:]
+    image_data = base64.b64decode(base64_string)
+    image = Image.open(io.BytesIO(image_data))
+    return image
+
+# Function to process the dataframe
+def process_images(dataframe, number_of_entries):
+    # Assuming the 5th column has index 4 (0-based indexing)
+    for base64_string in dataframe.iloc[:number_of_entries, 4]:
+        image = base64_to_image(base64_string)
+        image.show() # Display the image
+        # image.save("filename.png")
+
+# Read the CSV file (or any other data source)
+data = pd.read_csv("./emojis.csv")
+
+# Process the first X entries of the 5th column
+X = 5  
+process_images(data, X)
